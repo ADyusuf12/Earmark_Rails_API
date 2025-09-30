36a6855455bcade34de75eebecf0bcda26162ed0
ADyusuf12 <adyusuf68@gmail.com>
2025-09-30 19:27:19 +0100

    lint

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,14 +9,13 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key
     jwt.dispatch_requests = [
-      ['POST', %r{^/api/v1/login$}]
+      [ "POST", %r{^/api/v1/login$} ]
     ]
     jwt.revocation_requests = [
-      ['DELETE', %r{^/api/v1/logout$}]
+      [ "DELETE", %r{^/api/v1/logout$} ]
     ]
     jwt.expiration_time = 1.day.to_i
     jwt.request_formats = { user: [:json] }
